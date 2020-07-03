091e84b150399c87fe9b4034663a4e636c7ab87f
successful_fella <preetjuneja@zingalbox.com>
2020-07-03 21:17:21 +0530

    Manually Add ID Cause Instagram Blocking on Server

--- add.php
+++ add.php
@@ -4,18 +4,18 @@
 		$username = $_POST['instagram'];
 		$id_json = file_get_contents(__DIR__.'/accounts.json');
 		$id_arr = json_decode($id_json, true);
-		$insta_resp = json_decode(file_get_contents("https://www.instagram.com/$username/?__a=1"));
-		$id = $insta_resp->graphql->user->id;
-		if($insta_resp->graphql->user->is_private) {
-			echo "Account private!"; die;
-		}
+		// $insta_resp = json_decode(file_get_contents("https://www.instagram.com/$username/?__a=1"));
+		// $id = $insta_resp->graphql->user->id;
+		// if($insta_resp->graphql->user->is_private) {
+		// 	echo "Account private!"; die;
+		// }
 		$username = '@'.$username;
 		foreach ($id_arr as $id_old => $username_old) {
 			if($username_old == $username) {
 				echo "Username already exists"; die;
 			}
 		}
-		$id_arr[$id] = $username;
+		$id_arr[$_POST['instagram_id']] = $username;
 		file_put_contents('accounts.json', json_encode($id_arr));
 		echo "User added to analytics";
 	}
@@ -28,7 +28,8 @@
 </head>
 <body>
 	<form method="POST">
-		<input type="text" name="instagram" placeholder="Instagram Username">
+		<input type="text" name="instagram" id="username" placeholder="Instagram Username"> <button type="button" onclick="window.open('https://www.instagram.com/'+document.getElementById('username').value+'/?__a=1', '_blank')">Get ID</button><br>
+		<input type="text" name="instagram_id" placeholder="Instagram ID"><br>
 		<button type="submit">Add</button>
 	</form>
 	<p>Update Lastest Data: </p><button type="button" onclick="this.innerHTML='Please wait...';this.disabled=true;window.location.href='update.php'">Update</button>
