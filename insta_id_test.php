ffe5c845f7437db6edf6966080f495d7585241d2
successful_fella <preetjuneja@zingalbox.com>
2020-07-05 14:34:05 +0530

    Try with AJAX

--- insta_id_test.php
+++ insta_id_test.php
@@ -1,32 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insta ID Grabbu</title>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
 
-	$username = "successful_fella";
-	$url = "https://www.instagram.com/$username/?__a=1";
+	<input type="text" placeholder="Enter Insta Username" id="username">
+	<br>
+	<button onclick="getID()">Grabbu</button>
+	<br><br>
+	<textarea id="op"></textarea>
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL,$url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	$headers = [
-	    'authority: www.instagram.com',
-	    'method: GET',
-	    "path: /$username/?__a=1",
-	    'scheme: https',
-	    'accept: application/json, text/plain, */*',
-		// 'accept-encoding: gzip, deflate, br',
-	    'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
-	    'origin: http://localhost',
-		'referer: http://localhost/instagram',
-		'sec-fetch-dest: empty',
-		'sec-fetch-mode: cors',
-		'sec-fetch-site: cross-site',
-		'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-	];
-
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-	$server_output = curl_exec($ch);
-
-	curl_close($ch);
-
-	print  $server_output;
+	<script type="text/javascript">
+		function getID() {
+			$.ajax({
+				url: "https://www.instagram.com/successful_fella/?a=__1",
+				success: function(resp) {
+					$('#op').val(resp)
+				}
+			})
+			   
+		}
+	</script>
+</body>
+</html>
